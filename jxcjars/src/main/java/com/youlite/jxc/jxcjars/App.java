7b1f3c007c27670a24f2ac0039d4a9790ef726c8
David <guowei@hkfdt.cn>
2015-11-25 16:42:47 +0800

    add jxcjars

---
+++ jxcjars/src/main/java/com/youlite/jxc/jxcjars/App.java
@@ -0,0 +1,13 @@
+package com.youlite.jxc.jxcjars;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
